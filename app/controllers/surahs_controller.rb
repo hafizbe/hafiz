71929d62706510a25ed34758fbfa824f63d86415
hafizbe <hafizbe@gmail.com>
2014-01-17 12:25:24 +0100

    Fix button without submit

--- app/controllers/surahs_controller.rb
+++ app/controllers/surahs_controller.rb
@@ -12,4 +12,16 @@ class SurahsController < ApplicationController
     render "show"
   end
 
+  def show
+    surah_id = nil
+    if params[:id].nil?
+      surah_id = 1
+    else
+      surah_id = params[:id]
+    end
+    surah = Surah.find(surah_id)
+    @surah_presenter = SurahPresenter.new(surah,1,7)
+    render "show"
+  end
+
 end
